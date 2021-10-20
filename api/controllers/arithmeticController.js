eb7fb56d8a1901401c8d23de76763ca06f001134
Restyled.io <commits@restyled.io>
2021-10-20 02:40:36 +0000

    Restyled by prettier

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -1,20 +1,28 @@
-'use strict';
+"use strict";
 
-exports.calculate = function(req, res) {
-  req.app.use(function(err, req, res, next) {
+exports.calculate = function (req, res) {
+  req.app.use(function (err, req, res, next) {
     if (res.headersSent) {
       return next(err);
     }
 
     res.status(400);
-    res.json({error : err.message});
+    res.json({ error: err.message });
   });
 
   var operations = {
-    'add' : function(a, b) { return a + b },
-    'subtract' : function(a, b) { return a - b },
-    'multiply' : function(a, b) { return a * b },
-    'divide' : function(a, b) { return a / b },
+    add: function (a, b) {
+      return a + b;
+    },
+    subtract: function (a, b) {
+      return a - b;
+    },
+    multiply: function (a, b) {
+      return a * b;
+    },
+    divide: function (a, b) {
+      return a / b;
+    },
   };
 
   // Determine the operation
@@ -31,20 +39,24 @@ exports.calculate = function(req, res) {
 
   // Validate operands
 
-  if (!req.query.operand1 ||
-      !req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand1.replace(/[-0-9e]/g, '').length > 1) {
+  if (
+    !req.query.operand1 ||
+    !req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+    req.query.operand1.replace(/[-0-9e]/g, "").length > 1
+  ) {
     throw new Error("Invalid operand1: " + req.query.operand1);
   }
 
-  if (!req.query.operand2 ||
-      !req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand2.replace(/[-0-9e]/g, '').length > 1) {
+  if (
+    !req.query.operand2 ||
+    !req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+    req.query.operand2.replace(/[-0-9e]/g, "").length > 1
+  ) {
     throw new Error("Invalid operand2: " + req.query.operand2);
   }
 
   var operand1 = parseInt(req.query.operand1, 10);
   var operand2 = parseInt(req.query.operand2, 10);
 
-  res.json({result : operation(req.query.operand1, req.query.operand2)});
+  res.json({ result: operation(req.query.operand1, req.query.operand2) });
 };
